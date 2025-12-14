efd8f8f5cdef8371d8549b9fe384e9c93118b770
joaop1109 <joaopedropiresqueiroz10@gmail.com>
2025-12-14 12:40:59 -0300

    retirando mouse do hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -85,13 +85,8 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white rounded-full flex items-start justify-center p-2">
-          <div className="w-1 h-3 bg-white rounded-full"></div>
-        </div>
-      </div>
+      
+      {/* O código do Scroll Indicator (mouse) foi removido daqui */}
     </section>
   );
 };
